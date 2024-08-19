491b34a0222662a8cfca06c904d1b143a28cc401
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-08-19 21:07:47 +0100

    sensei interfaces built features : - add student - plan event

---
+++ database/migrations/2024_07_26_091048_create_disciplines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Enums\DojoStatus;
+use App\Models\Discipline;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(Discipline::TABLE_NAME, function (Blueprint $table) {
+
+            $table->id();
+            $table->string(Discipline::NAME)->unique();
+            $table->string(Discipline::STATUS)->default(DojoStatus::ACTIVE);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(DISCIPLINE::TABLE_NAME);
+    }
+};
